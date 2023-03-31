1a03639b828cf3525611e44f101ab494347e806d
Muhammad Hamza <m.hamza@axiomworld.net>
2023-03-31 21:20:20 +0500

    {UPDATE} file

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@ set -u
 set -e
 cd /Odoo/env/env15/addons/new_pipeline_test
 git pull origin master
-systemctl stop odoo
+#systemctl stop odoo
 #source /Odoo/env/env15/bin/activate
 #/Odoo/src/odoo/odoo-bin -c /Odoo/env/env15/config/env15.conf -i account --stop-after-init
 #deactivate
-systemctl start odoo
+#systemctl start odoo
+service nginx restart
